bc98e46b4d94c8b9d94dcab367ab18204b37422b
Kurt Nelson <kurtisnelson@gmail.com>
2015-10-14 11:21:09 -0500

    Players can call shots

--- computer_player.rb
+++ computer_player.rb
@@ -2,4 +2,26 @@ class ComputerPlayer < Player
   def initialize
     super("HAL 9000")
   end
+
+  def place_ships lengths
+    lengths.each do |length|
+      until place_random_ship(length) do; end
+    end
+    puts "HAL 9000 has placed its ships."
+  end
+
+  def place_random_ship length
+    rnd = Random.new
+    x = rnd.rand(10) + 1
+    y = rnd.rand(10) + 1
+    horizontal = rnd.rand(2) == 0
+    grid.place_ship Ship.new(length), x, y, horizontal
+  end
+
+  def call_shot
+    rnd = Random.new
+    x = (rnd.rand(10) + 65).chr
+    y = rnd.rand(10) + 1
+    x.to_s + y.to_s
+  end
 end
--- human_player.rb
+++ human_player.rb
@@ -1,5 +1,14 @@
+def get_user_input
+  gets.chomp
+end
+
 class HumanPlayer < Player
   def initialize name = "Dave"
     super(name)
+  end
+
+  def call_shot
+    puts "#{name}, please enter the coordinates for your next shot (e.g. 'B10'):"
+    get_user_input
   end
 end
