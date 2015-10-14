11926f72d1e09eb5d3821742b999aedf386d6582
Kurt Nelson <kurtisnelson@gmail.com>
2015-10-14 10:34:56 -0500

    Grid with ships can display

--- grid.rb
+++ grid.rb
@@ -1,6 +1,10 @@
 class Grid
+  def initialize
+    @ships = []
+  end
+
   def has_ship_on? x, y
-    false
+    @ships.find{|ship| ship.covers? x, y}
   end
 
   def display
@@ -12,6 +16,15 @@ class Grid
     puts seperator
   end
 
+  def place_ship ship, x, y, horizontal
+    ship.place(x, y, horizontal)
+    if @ships.find {|grid_ship| grid_ship.overlaps_with? ship}
+      false
+    else
+      @ships << ship
+    end
+  end
+
   private
 
   def seperator
@@ -19,6 +32,19 @@ class Grid
   end
 
   def grid_line letter
-    "#{letter} |   |   |   |   |   |   |   |   |   |   |"
+    y = letter_to_y(letter)
+    slots = []
+    (1..10).each do |x|
+      if has_ship_on? x, y
+        slots << "O"
+      else
+        slots << " "
+      end
+    end
+    "#{letter} | #{slots.join(" | ")} |"
+  end
+
+  def letter_to_y ltr
+    ltr.upcase.ord - 64
   end
 end
